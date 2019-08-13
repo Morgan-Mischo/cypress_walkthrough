12a62ababc052b4c5370fbb4abc66e84daaff25f
Morgan-Mischo <goramorgan@gmail.com>
2019-08-13 12:25:12 -0600

    finished mini

---
+++ cypress/integration/input_form.spec.js
@@ -0,0 +1,34 @@
+describe('Input form', () => {
+    const textInput = 'Buy milk'
+    beforeEach( () => {
+        cy.visit('/')
+    });
+
+    it('Accepts user input', () => {
+        cy.get('[data-cy-input]')
+        .type(textInput)
+        .should('have.value', textInput)
+    });
+
+    it('Submits a new todo', () => {
+        cy.get('[data-cy-input]')
+        .type(textInput)
+        .type('{enter}')
+        .should('have.value', '')
+
+        cy.get('[data-cy-input]')
+        .type(textInput)
+
+        cy.get('[data-cy-submit]')
+        .click()
+
+        cy.get('[data-cy-input]')
+        .should('have.value', '')
+
+        cy.get('.todos')
+        .should('be.visible')
+        .should('have.length', 6)
+        .eq(-1)
+        .should('contain', textInput)
+    }); 
+}); 
